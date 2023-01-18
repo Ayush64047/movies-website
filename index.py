e75b0b558fa2264920c35a0b3f488526be5f4b4e
Ayush64047 <bhaskarsharma64047@gmail.com>
2023-01-19 00:13:50 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://movies-website-gules.vercel.app"
+URL = "https://movies-website-sand.vercel.app"
 bot = Bot(TOKEN)
 
 
